0db947686baaab94214163f2524c099421eb2578
Peter Toth <tothpeter08@gmail.com>
2017-04-30 02:17:28 +0200

    Dynamic Changes With State

--- react-native/tutsplus_get_started/index.ios.js
+++ react-native/tutsplus_get_started/index.ios.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 import RunInfo from './components/run-info';
+import RunInfoNumeric from './components/run-info-numeric';
 
 const styles = StyleSheet.create({
   infoWrapper: {
@@ -15,14 +16,36 @@ const styles = StyleSheet.create({
 });
 
 export default class tutsplus_get_started extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    setInterval(() => {
+      this.distanceInfo.setState({ value: Math.random() * 100 })
+      this.speedInfo.setState({ value: Math.random() * 10 })
+      this.directionInfo.setState({
+        value: this.directionInfo.state.value === 'N' ? 'NW':'N'
+      })
+    }, 500);
+  }
+
   render() {
     return (
       <View style={{flex: 1}}>
         <Text style={{flex: 1, backgroundColor: 'red'}}>MAPVIEW</Text>
         <View style={styles.infoWrapper}>
-          <RunInfo title="Distance" value="0 km" />
-          <RunInfo title="Speed" value="0 km/h" />
-          <RunInfo title="Direction" value="NE" />
+          <RunInfoNumeric title="Distance"
+            unit="km"
+            ref={(info) => this.distanceInfo = info}
+            />
+          <RunInfoNumeric title="Speed"
+            unit="km/h"
+            ref={(info) => this.speedInfo = info}
+          />
+          <RunInfo title="Direction"
+            value="NE"
+            ref={(info) => this.directionInfo = info}
+          />
         </View>
       </View>
     );
