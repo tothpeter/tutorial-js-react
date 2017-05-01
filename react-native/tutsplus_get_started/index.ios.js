7b61f1e5cf6dae8301a15820996c3aaddf8f837b
Peter Toth <tothpeter08@gmail.com>
2017-05-01 13:47:40 +0200

    Geolocation API

--- react-native/tutsplus_get_started/index.ios.js
+++ react-native/tutsplus_get_started/index.ios.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 
 import MapView from 'react-native-maps';
+import haversine from 'haversine';
 
 import RunInfo from './components/run-info';
 import RunInfoNumeric from './components/run-info-numeric';
@@ -26,15 +27,53 @@ let id = 0;
 export default class tutsplus_get_started extends Component {
   constructor(props) {
     super(props);
-    this.state = { markers: [] };
 
-    setInterval(() => {
-      this.distanceInfo.setState({ value: Math.random() * 100 })
-      this.speedInfo.setState({ value: Math.random() * 10 })
-      this.directionInfo.setState({
-        value: this.directionInfo.state.value === 'N' ? 'NW':'N'
+    let watchID = navigator.geolocation.watchPosition((position) => {
+      this.speedInfo.setState({ value: position.coords.speed })
+
+      let distance = 0;
+
+      if (this.state.previousCoordinate) {
+        distance = this.state.distance + haversine(this.state.previousCoordinate, position.coords);
+      }
+
+      this.distanceInfo.setState({value: distance})
+
+      let x = position.coords.heading;
+      if ((x > 0 && x <= 23) || (x > 338 && x <= 360))
+        this.directionInfo.setState({ value: 'N' });
+      else if (x > 23 && x <= 65)
+        this.directionInfo.setState({ value: 'NE' });
+      else if (x > 65 && x <= 110)
+        this.directionInfo.setState({ value: 'E' });
+      else if (x > 110 && x <= 155)
+        this.directionInfo.setState({ value: 'SE' });
+      else if (x > 155 && x <= 203)
+        this.directionInfo.setState({ value: 'S' });
+      else if (x > 203 && x <= 248)
+        this.directionInfo.setState({ value: 'SW' });
+      else if (x > 248 && x <= 293)
+        this.directionInfo.setState({ value: 'W' });
+      else if (x > 293 && x <= 338)
+        this.directionInfo.setState({ value: 'NW' });
+
+      this.setState({
+        markers: [
+          ...this.state.markers, {
+            coordinate: position.coords,
+            key: id++
+          }
+        ],
+        previousCoordinate: position.coords,
+        distance
       })
-    }, 1000);
+    }, null, { distanceFilter: 10 });
+
+    this.state = { markers: [], watchID };
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.state.watchID);
   }
 
   addMarker(region) {
@@ -62,11 +101,11 @@ export default class tutsplus_get_started extends Component {
         <MapView style={styles.map}
           showsUserLocation
           followsUserLocation
-          onRegionChange={(region) => this.addMarker(region) }
         >
-          {this.state.markers.map((marker) => (
-            <MapView.Marker coordinate={marker.coordinate} key={marker.key} />
-          ))}
+          <MapView.Polyline
+            coordinates={this.state.markers.map((marker) => marker.coordinate )}
+            strokeWidth={5}
+          />
         </MapView>
         <View style={styles.infoWrapper}>
           <RunInfoNumeric title="Distance"
